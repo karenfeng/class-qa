7c25f68c5d4b1b9897fdfdf3d3b12efc5304bade
mwang221 <margaretdwang@gmail.com>
2016-03-28 20:51:16 -0400

    Update forms.py
    Add an answer form

--- quailapp/forms.py
+++ quailapp/forms.py
@@ -2,3 +2,6 @@ from django import forms
 
 class QuestionForm(forms.Form):
     your_question = forms.CharField(label='Your question', max_length=100)
+
+class AnswerForm(forms.Form):
+    your_answer = forms.CharField(label='Your answer', max_length=200)
