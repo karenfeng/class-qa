7ba6d544159b4ebafe285d3858ce1d2703b5a3b8
Karen Feng <karenfeng.us@gmail.com>
2016-04-01 09:24:05 -0400

    Static files fix

--- apps/settings.py
+++ apps/settings.py
@@ -119,7 +119,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/staticfiles/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
