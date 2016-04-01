e24b87c50256044a744d17eadcddb31750e917e8
Karen Feng <karenfeng.us@gmail.com>
2016-04-01 01:52:02 -0400

    Fit static file settings

--- apps/settings.py
+++ apps/settings.py
@@ -118,7 +118,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
