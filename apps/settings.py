1bb3c61484dc88ab98ff244444ce8973ce403f42
Karen Feng <karenfeng.us@gmail.com>
2016-04-01 01:56:59 -0400

    Trying to fix remote heroku static settings

--- apps/settings.py
+++ apps/settings.py
@@ -119,7 +119,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
